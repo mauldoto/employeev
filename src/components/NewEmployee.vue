72de9b8ae290849ea050a468a1bf766595837a46
mauldoto <mauldoto@gmail.com>
2020-03-25 21:42:31 +0700

    adding new component

---
+++ src/components/NewEmployee.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="new-employee">
+        <h3>New Employee</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'new-employee',
+    data(){
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
