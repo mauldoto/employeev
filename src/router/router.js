c9702acc9d9a2477943a14c1bb00fd525575d545
mauldoto <mauldoto@gmail.com>
2020-03-25 21:42:17 +0700

    setup route vue

---
+++ src/router/router.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Dashboard from '../components/Dashboard.vue'
+import NewEmployee from '../components/NewEmployee.vue'
+import ViewEmployee from '../components/ViewEMployee.vue'
+import EditEmployee from '../components/EditEmployee.vue'
+
+Vue.use(Router)
+
+export default new Router({
+    routes: [
+        {
+            path: '/',
+            name: 'dashboard',
+            component: Dashboard
+        },
+        {
+            path: '/new',
+            name: 'new-employee',
+            component: NewEmployee
+        },
+        {
+            path: '/:employee_id',
+            name: 'view-employee',
+            component: ViewEmployee
+        },
+        {
+            path: '/edit/:employee_id',
+            name: 'edit-employee',
+            component: EditEmployee
+        }
+    ]
+})
